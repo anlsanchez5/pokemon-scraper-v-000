56c4161383bb0172b9215bd9d41fb286295b668f
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-04 19:30:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,17 +15,17 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
-  def self.add_hp(db)
-    db.execute("ALTER TABLE pokemon ADD COLUMN hp INTEGER")
-    db.execute("UPDATE pokemon SET hp = 60")
-    @@all.each do |pokemon|
-      pokemon.hp = 60
-    end
-  end
+#  def self.add_hp(db)
+#    db.execute("ALTER TABLE pokemon ADD COLUMN hp INTEGER")
+#    db.execute("UPDATE pokemon SET hp = 60")
+#    @@all.each do |pokemon|
+#      pokemon.hp = 60
+#    end
+#  end
 
 
   def self.find(id, db)
-    add_hp(db)
+#    add_hp(db)
     pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     self.new(id:pokemon_from_db[0][0], name:pokemon_from_db[0][1], type:pokemon_from_db[0][2], db:db, hp:pokemon_from_db[0][2])
   end
