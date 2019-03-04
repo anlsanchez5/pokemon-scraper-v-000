c2d8e5de84da499842327d89bc8751a369176609
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-04 21:05:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,6 +17,7 @@ class Pokemon
 
 
   def self.find(id, db)
+    db.execute("UPDATE pokemon SET "), id;
     pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
     self.new(id:pokemon_from_db[0][0], name:pokemon_from_db[0][1], type:pokemon_from_db[0][2], db:db, hp:pokemon_from_db[0][2])
   end
