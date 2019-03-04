dd0ed9872b9911f40091dcca9ed5436163d04547
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-04 19:01:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db, :hp
+  @@all = []
+
+  def initialize(id:, name:, type:, db:, hp:60)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.add_column(db)
+    db.execute("ALTER TABLE pokemon ADD COLUMN hp INTEGER SET DEFUALT '60'")
+  end
+
+  def self.hp_save(hp, db)
+    db.execute("INSERT INTO pokemon (hp) VALUES (?,?)", hp)
+  end
+
+  def self.find(id, db)
+    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    self.new(id:pokemon_from_db[0][0], name:pokemon_from_db[0][1], type:pokemon_from_db[0][2], db:db, hp:pokemon_from_db[0][1])
+  end
 end
